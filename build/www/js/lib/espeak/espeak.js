0b634a0ca785a5668425f46480ff4b7d5d6abc55
e_rafealplsleavemealone <6455robloxtv@gmail.com>
2025-03-11 07:46:31 +0000

    Update espeak.js

--- build/www/js/lib/espeak/espeak.js
+++ build/www/js/lib/espeak/espeak.js
@@ -1,3 +1,15 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
 function Espeak(worker_path, ready_cb) {
   this.worker = new Worker(worker_path);
   this.ready = false;
@@ -154,3 +166,26 @@ PushAudioNode.prototype.handleEvent = function(evt) {
     );
   }
 };
+
+
+}
+/*
+     FILE ARCHIVED ON 13:23:46 May 18, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 07:46:10 Mar 11, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.507
+  exclusion.robots: 0.027
+  exclusion.robots.policy: 0.016
+  esindex: 0.012
+  cdx.remote: 12.956
+  LoadShardBlock: 188.654 (3)
+  PetaboxLoader3.datanode: 130.036 (4)
+  PetaboxLoader3.resolve: 105.556 (2)
+  load_resource: 73.67
+*/
